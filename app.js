5b0150f2f149e04bf83f4f739b905ea9803308fd
isalevine <isa.loves.coding@gmail.com>
2019-08-06 14:32:42 -0700

    building output and Owner data structures

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const csv = require('csv-parser');
 const fs = require('fs');
+ 
 
-const results = [];
+const output = {
+    "date": "",
+    "cash_raised": 0.00,
+    "total_number_of_shares": 0,
+    "ownership": []
+}
+
+function Owner(investor, shares, cash_paid, ownership) {
+    this.investor = investor;    // String
+    this.shares = shares;        // Integer
+    this.cash_paid = cash_paid;  // Decimal
+    this.ownership = ownership;   // Decimal (percentage)
+}
+
+const results = [];   
 
 fs.createReadStream('example_data.csv')
     .pipe(csv())
-    .on('data', (data) => results.push(data))
+    .on('data', (data) => {
+        results.push(data)
+    })
     .on('end', () => {
         console.log('results[0]: ', results[0])
     })
-    
-
