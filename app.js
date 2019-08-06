0e46ff1e745f351da53776bf89522df80b2520e7
isalevine <isa.loves.coding@gmail.com>
2019-08-05 21:25:33 -0700

    changed to using csv-parser package instead of csv-parse

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const parse = require('csv-parse/lib/sync')
+const csv = require('csv-parser');
+const fs = require('fs');
 
-// const input = 
+const results = [];
+
+fs.createReadStream('example_data.csv')
+    .pipe(csv())
+    .on('data', (data) => results.push(data))
+    .on('end', () => {
+        console.log('results[0]: ', results[0])
+    })
+    
+
